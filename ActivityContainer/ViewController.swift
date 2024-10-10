a2d52dddda2833b298b9c9adada76ca438ab3a46
Michael <makoy.ramirez@gmail.com>
2024-10-10 14:39:39 +0800

    Initial Commit

---
+++ ActivityContainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityContainer
+//
+//  Created by Macky Ramirez on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
